b6c75511d5875991133e1af612c72e1f67dbd014
YannickPire <yannick.pire@gmail.com>
2024-04-09 20:40:39 +0200

    Initial commit

---
+++ src/test/java/be/personal/tutorials/testcontainer/TestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.personal.tutorials.testcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
